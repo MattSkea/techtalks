bb38bdd86ad79ea906cbb23d2da2d483180636ed
trix0 <matus.satara@gmail.com>
2017-05-24 17:14:58 +0200

    fiex event.php

---
+++ content/event.php
@@ -0,0 +1,49 @@
+S<div id="section-event" class="section">
+
+
+	<!-- Margin container-->
+	<div class="margin-container">
+		<div class="event-container">
+			<div class="event-card">
+				<img class="event-card-image" src="img/content/test-img-320x200.jpg" alt="Event Image">
+				<div class="event-card-text-container">
+    			<h4>Copenhagen AngularJS Meet-up</h4> 
+    			<h4>Getting Started With Angular 2 Cli</h4>
+    			<div class="event-card-content-big">
+	    			<p class="event-card-text-paragraph">Angular 2 is released and it is time to start building your new apps with it. We will do an introduction to Angular 2 CLI by creating a new web application and explore how the tool can help you in your development. Depending on the audience there will be room for some more advanced stuff. </p> 
+	    			<div class="event-card-image-big">
+	    				<img class="event-card-image-big-img" src="img/content/test-img-320x200.jpg" alt="Event Image">
+	    			</div>
+    			</div>
+  			</div>
+  			<div class="event-card-content-time-container">
+	  			<div class="event-card-content-time">
+	  				<span class="event-card-content-time-icon"></span>
+	  				<div class="event-card-content-time-text">Thursday 09 Feb 18:30-22:00</div>
+					</div>
+					<div data-button="event-show-map" class="buttonClick event-card-content-location">
+	  				<span class=" event-card-content-location-icon"></span>
+	  				<div class=" event-card-content-location-text">Founders - Skelbækgade 2 1717 København</div>
+					</div>
+					<div class="event-card-map-container">
+						<img src="img/content/test-img-map-320x320.jpg">
+					</div>
+				</div>
+				<div class="event-card-content-pricing">
+  				<span class="event-card-content-pricing-price">
+  					200 DKK
+  				</span>
+  				<div class="event-card-content-pricing-button">
+  					<div class="button-green button" >Book now</div>
+  				</div>
+				</div>
+				<div class="event-card-content-tags">
+  				<div class="event-card-content-tags-tags">angularJS, Ionic, lorem, ipsum,</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+</div>
