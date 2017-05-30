ae96d023b7c164e4d32ffe6281576a3d51b081ae
Matt Skea <mattsskea@gmail.com>
2017-05-30 14:47:48 +0200

    - finished initial user register
    - registers users

--- services/api-user-create.php
+++ services/api-user-create.php
@@ -2,11 +2,21 @@
 
 include '../inc/db.php';
 
-$fname = $_POST["fname"];
-$lname = $_POST["lname"];
 $email = $_POST["email"];
 $pass = $_POST["pass"];
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$mobile = $_POST['mobile'];
 
 print_r($_POST);
+$stmt = $db->prepare("CALL CreateSystemUser(:email, :pass, :fname, :lname, :mobile)");
+
+$stmt->bindValue(":email", $email);
+$stmt->bindValue(":pass", $pass);
+$stmt->bindValue(":fname", $fname);
+$stmt->bindValue(":lname", $lname);
+$stmt->bindValue(":mobile", $mobile);
+
+$stmt->execute();
 
 echo "{type:success}";
